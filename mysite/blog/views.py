9b8486707fc396df5e2dd3d83fd4924cb12ff270
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2025-02-20 15:06:27 +0000

    added functionality to post comments on posts and display them

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,12 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
-from .models import Post
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from .forms import EmailPostForm
-from django.core.mail import send_mail
+from .forms import CommentForm, EmailPostForm
+
+from .models import Post
+
 
 class PostListView(ListView):
     """
@@ -31,7 +34,19 @@ def post_detail(request, year,month,day,post):
         publish__year=year,
         publish__month=month,
         publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'form': form,                    
+        }
+        )
 
 
 def post_share(request, post_id):
@@ -78,3 +93,22 @@ def post_share(request, post_id):
             'sent': sent
             }
         )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request, 
+        'blog/post/comment.html', 
+        {
+            'post': post,
+            'form': form,
+            'comment': comment,
+        }
+        )    
