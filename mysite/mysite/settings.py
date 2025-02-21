5c4c74c5a1259bb0fac82d2d4b788b6bace976b7
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2025-02-21 12:26:52 +0000

    sitemap functionality

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -45,6 +46,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     "django.contrib.staticfiles",
     "taggit",
     "blog.apps.BlogConfig",
