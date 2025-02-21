1ea4ffb98fd76706d9540821f678c6ca7d5b0421
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2025-02-21 10:16:39 +0000

    function added:tags

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishManager(models.Manager):
@@ -44,6 +45,8 @@ class Post(models.Model):
             "blog:post_detail",
             args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
+    
+    tags = TaggableManager()
 
 
 class Comment(models.Model):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from .forms import CommentForm, EmailPostForm
 
 from .models import Post
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -19,13 +20,23 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
     posts = paginator.get_page(page_number)
 
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request,
+                  "blog/post/list.html",
+                  {
+                    "posts": posts,
+                    "tag": tag,
+                    }
+                  )
 
 
 def post_detail(request, year, month, day, post):
